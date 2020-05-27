fad32f592203b60de24bf7c5d3b6cd3277ee2e48
tral909 <indorm1992@mail.ru>
2020-05-27 11:54:50 +0300

    Add addition components: error-boundry, context, hoc for work with service withXyzService

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+  return (
+    <p>App comp</p>
+  );
+};
+
+export default App;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <p>loading...</p>
+  );
+};
+export default Spinner;
