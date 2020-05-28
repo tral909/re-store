832c57a4724a30c0036c7329cb5da3c1c217fd0a
tral909 <indorm1992@mail.ru>
2020-05-28 09:50:56 +0300

    Add styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import ShopHeader from '../shop-header';
+import ShoppingCartTable from '../shopping-cart-table';
 import './app.css';
 import {
   HomePage,
@@ -8,11 +10,16 @@ import {
 
 const App = () => {
   return (
-    // Отрисовывает максимум один Route
-    <Switch>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/cart/" component={CartPage} exact />
-    </Switch>
+    <main role="main" className="container">
+      <ShopHeader numItems={5} total={210} />
+      {// Отрисовывает максимум один Route
+      }
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/cart/" component={CartPage} exact />
+      </Switch>
+      <ShoppingCartTable />
+    </main>
   );
 };
 
